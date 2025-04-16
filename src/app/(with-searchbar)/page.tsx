2c2aada8d46a41caf7966bc589ae21cddd15e490
Jim Kim <icekjm1029@gmail.com>
2025-04-16 22:17:52 +0900

    feat:Error Handling

--- src/app/(with-searchbar)/page.tsx
+++ src/app/(with-searchbar)/page.tsx
@@ -38,9 +38,9 @@ async function AllBooks() {
 async function RecoBooks() {
   await delay(3000);
   const response = await fetch(
-    `${process.env.NEXT_PUBLIC_API_SERVER_URL}/book/random`,
+    `${process.env.NEXT_PUBLIC_API_SERVER_URL}/book/random`
     // { cache: "force-cache" }
-    { next: { revalidate: 3 } }
+    // { next: { revalidate: 3 } }
   );
   if (!response.ok) {
     return <div>오류가 발생했습니다...</div>;
